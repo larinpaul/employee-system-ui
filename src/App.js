2109739a5c3e246e131ac618f27cb702c6e8a3f5
Pavel Larin <www.larinpavel@mail.ru>
2023-11-26 15:10:16 +0500

    An almost complete application

--- src/App.js
+++ src/App.js
@@ -1,8 +1,31 @@
+import { BrowserRouter } from 'react-router-dom';
 import './App.css';
+import AddEmployee from './components/AddEmployee';
+import Navbar from './components/Navbar';
+import EmployeeList from './components/EmployeeList';
 
 function App() {
-  return <div className="App"> </div>;
-  
+  return (
+    <>
+    <BrowserRouter>
+        <Navbar />
+        <Routes>
+            {/* <Route path="/" element={<EmployeeList />}></Route>
+            <Route index element={<EmployeeList />}></Route>
+            <Route path="/employeeList" element={<EmployeeList />}></Route>
+            <Route path="/addEmployee" element={<AddEmployee />}></Route>     */}
+            <Route path="/" element={<EmployeeList />}/>
+            <Route index element={<EmployeeList />}></Route>
+            <Route path="/employeeList" element={<EmployeeList />}/>
+            <Route path="/addEmployee" element={<AddEmployee />}/>   
+            <Route 
+                path="/editEmployee/:id"
+                element={<UpdateEmployee />}
+            />
+        </Routes>
+    </BrowserRouter>
+    </>
+  ); 
 }
 
 export default App;
